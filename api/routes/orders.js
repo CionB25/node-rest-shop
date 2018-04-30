f02170ef5989f2de894f73cfa9ed3d93921549f2
Kyle Andrews <kylemandrews@gmail.com>
2018-04-29 23:56:10 -0400

    Verify Product Prior to Saving Order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const mongoose = require('mongoose')
 const Order = require('../models/order')
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
   Order.find()
@@ -32,31 +33,41 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-  const order = new Order({
-    _id: new mongoose.Types.ObjectId(),
-    quantity: req.body.quantity,
-    product: req.body.productId
-  })
-  order
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: 'Order stored',
-        createdOrder: {
-          _id: result._id,
-          product: result.product,
-          quantity: result.quantity
-        },
-        request: {
-          type: 'GET',
-          url: 'http://localhost:3000/orders/' + result._id
-        }
-      });
+  Product.findById(req.body.productId)
+    .then(product => {
+      const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+      })
+      order
+      .save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: 'Order stored',
+          createdOrder: {
+            _id: result._id,
+            product: result.product,
+            quantity: result.quantity
+          },
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/orders/' + result._id
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        })
+      })
+
     })
     .catch(err => {
-      console.log(err);
       res.status(500).json({
+        message: 'Product not found',
         error: err
       })
     })
